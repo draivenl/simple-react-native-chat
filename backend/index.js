9f6dc8891dc1e5e6fcd1f71c0a4edb2f0df88b38
Lisandro Gomez <draivenl@gmail.com>
2019-11-21 18:55:22 -0500

    Backend response

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,10 @@ const PORT = 3000
 
 io.on("connection", socket => {
     console.log("a user connected :D")
+    socket.on("message", msg => {
+        console.log(msg);
+        io.emit('message', msg)
+    })
 })
 
 server.listen(PORT, ()=> console.log(`Server running on port: ${PORT}`))
