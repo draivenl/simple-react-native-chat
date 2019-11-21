c20cd4176e5619bc41230ba6f5a0e55b42cd9fc5
Lisandro Gomez <draivenl@gmail.com>
2019-11-21 18:53:56 -0500

    Sending and receiving message to backend

--- simpleChat/App.js
+++ simpleChat/App.js
@@ -1,16 +1,62 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View, TextInput, FlatList} from 'react-native';
 import io from "socket.io-client";
 
 class App extends Component {
-  componentDidMount(){
-    const socket = io("http://10.0.8.86:3000")
+  //SOCKET_URL = "http://10.0.8.86:3000"
+  SOCKET_URL = "http://192.168.0.9:3000"
+  constructor(props) {
+    super(props)
+    this.state = {
+      chatMessage: "",
+      chatMessages: []
+    }
   }
+
+  componentDidMount(){
+    this.socket = io(this.SOCKET_URL)
+    this.socket.on('message', msg => {
+      this.setState({
+        chatMessages: [...this.state.chatMessages, msg]
+      })
+    })
+  }
+
+  handleChangeMessage = chatMessage => {
+    this.setState({
+      chatMessage
+    })
+  }
+  submitChatMessage = () => {
+    this.socket.emit("message", this.state.chatMessage)
+    this.setState({
+      chatMessage: ""
+    })
+  }
+
+  renderEmpty = () => {
+    return <Text>No Messages</Text>
+  }
+  renderItem = item => {
+    return <Text>{item}</Text>
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <TextInput 
+          autoCorrect={false}
+          onSubmitEditing={this.submitChatMessage}
+          onChangeText={ this.handleChangeMessage }
+          style={styles.input_message}
+          value={this.state.chatMessage}
+        />
+        <Text>Messages</Text>
+        <FlatList 
+              ListEmptyComponent={()=>this.renderEmpty()}
+              data={this.state.chatMessages} 
+              renderItem={({item})=>this.renderItem(item)}
+          />
       </View>
     );
   }
@@ -19,15 +65,15 @@ class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    padding: 15
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
+  input_message: {
+    borderColor: 'black',
+    borderWidth: 1,
+    height: 40
+
+  }
+
 });
 
 export default App
